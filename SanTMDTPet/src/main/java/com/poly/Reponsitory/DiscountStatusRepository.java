ef313e18dc3297a7118b20b0d159d38f4a72d89f
lytran14 <139949514+lytran14@users.noreply.github.com>
2024-10-09 00:55:30 +0700

    Voucher_client

---
+++ SanTMDTPet/src/main/java/com/poly/Reponsitory/DiscountStatusRepository.java
@@ -0,0 +1,9 @@
+package com.poly.Reponsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poly.Model.DiscountsStatus;
+
+public interface DiscountStatusRepository extends JpaRepository<DiscountsStatus, Integer> {
+
+}
---
+++ SanTMDTPet/src/main/java/com/poly/Reponsitory/DiscountTypeTRepository.java
@@ -0,0 +1,9 @@
+package com.poly.Reponsitory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.poly.Model.DiscountType;
+
+public interface DiscountTypeTRepository extends JpaRepository<DiscountType, Integer> {
+
+}
